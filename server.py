964ae0edddd0ae97b963843dd4fbbe07f548e8ff
repl.it user <replituser@example.com>
2021-03-07 09:31:10 +0000

    added logger

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from waitress import serve
+from paste.translogger import TransLogger
 
 app = Flask('')
 
@@ -8,13 +9,19 @@ app = Flask('')
 def home():
     return render_template('index.html')
 
+
 @app.route('/test')
 def test():
     return render_template('test.html')
 
 
 def run():
-    serve(app, host='0.0.0.0', port=8080, url_scheme='https', ident=None)
+    format_logger = '[%(time)s] %(status)s %(REQUEST_METHOD)s %(REQUEST_URI)s'
+    serve(TransLogger(app, format=format_logger),
+          host='0.0.0.0',
+          port=8080,
+          url_scheme='https',
+          ident=None)
 
 
 if __name__ == '__main__':
